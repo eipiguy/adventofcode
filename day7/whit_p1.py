2a6a10820cc845c836a7d4ac3f515fc3639c37ec
whitgroves <whitney.groves@gmail.com>
2020-12-12 15:56:47 -0600

    day7 part 1

---
+++ day7/whit_p1.py
@@ -0,0 +1,22 @@
+import re
+
+bag_map = {}
+patt = '(\w+ \w+) bag'  # thank you pythex.org!
+
+with open('input.txt', 'r') as infile:
+    for line in infile.readlines():
+        bags = re.findall(patt, line)
+        container = bags[0]
+        contained = [] if bags[-1] == 'no other' else bags[1:]
+        bag_map[container] = contained
+        
+def get_valid_containers(targets: set, container_map: dict) -> set:
+    res = set()
+    for t in targets:
+        res.update([k for k in container_map if t in container_map[k]])
+    if all(r in targets for r in res):
+        return res
+    return get_valid_containers(targets.union(res), container_map)
+
+valid_containers = get_valid_containers({'shiny gold'}, bag_map)
+print(len(valid_containers))
